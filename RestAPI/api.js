511d0e72ca18a6c3d033d8bcca7ef1d49ce591b5
Khasim Ali <khasim.ali@zensar.com>
2018-04-20 20:45:50 +0530

    elaquo changes

--- RestAPI/api.js
+++ RestAPI/api.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 var fs = require('file-system');
+var Eloqua = require('eloqua-request');
+var eloqua = new Eloqua('https://s1883512475.t.eloqua.com', 'user', 'pass');
+var bodyParser = require('body-parser');
+
 
 //Call Salesforce Login API
 router.get('/Login', (req, res) => {
@@ -26,6 +30,29 @@ router.get('/Login', (req, res) => {
 		} else{
 			res.send({"error":error});
 		}
+	});
+});
+
+router.post('/Eloqua/f2', (req, res) => {
+	console.log(req.body);
+	eloqua.post('https://s1883512475.t.eloqua.com/e/f2', req.body, function(err, response){
+		console.log("err"+err+"="+response);
+	});
+	request.post({
+		url:'https://s1883512475.t.eloqua.com/e/f2',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		body: JSON.stringify(req.body)
+	},
+	function(error, response, body){
+		console.log(body+"---->"+error+"=="+JSON.stringify(response));
+		if(!error && (response.statusCode == 200 || response.statusCode == 201)){
+			res.send(response.body);
+		} else{
+			res.send({"error":error});
+		}
+		res.end();
 	});
 });
 
